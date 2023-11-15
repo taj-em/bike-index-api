2142113c33243b6dd465f1ae9155539133956886
Your Name <tajempey@gmail.com>
2023-11-15 15:48:05 -0700

    Display the 10 most recently stolen bikes within a 40 mile radius of a selected location
    Co-authored by: Hunter Empey
    <LizardMak@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-function getBikesWithin() {
+function getBikesWithin(event) {
+  event.preventDefault();
+  const city = document.getElementById("userInput").value;
   let promise = new Promise(function (resolve, reject) {
     let request = new XMLHttpRequest();
-    const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=10&location=Chico%2C%20CA%2C%20US&distance=40&stolenness=proximity`;
+    const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=10&location=${city}&distance=40&stolenness=proximity`;
     request.addEventListener("loadend", function () {
       const response = JSON.parse(this.responseText);
       if (this.status === 200) {
-        resolve([response]);
+        resolve(response, city);
       } else {
-        reject([this, response]);
+        reject(this);
       }
     });
     request.open("GET", url);
@@ -16,12 +18,27 @@ function getBikesWithin() {
 
   promise.then(function (response) {
     console.log(response);
-    for (let i = 0; i < response[0].bikes.length; i++) {
-      document.getElementById("display-bike-manufacturer").append(response[0].bikes[i].manufacturer_name);
+    let displayDiv = document.getElementById("display-bikes");
+    for (let i = 0; i < response.bikes.length; i++) {
+      let h3 = document.createElement("h3");
+      let ul = document.createElement("ul");
+      let li = document.createElement("li");
+      h3.innerText = "Make/Model: " + response.bikes[i].title + " " + "Frame Color(s): " + response.bikes[i].frame_colors;
+      if (response.bikes[i].stolen_location) {
+        li.append("Stolen From: " + response.bikes[i].stolen_location);
+      } else {
+        li.append("Stolen From: Unknown Location");
+      }
+      ul.append(li);
+      displayDiv.append(h3);
+      displayDiv.append(ul);
     }
   }, function (errorMessage) {
-    console.log(errorMessage);
+    let errorDiv = document.getElementById("display-error");
+    let p = document.createElement("p");
+    p.append("Error Code: " + errorMessage.status + " => " + errorMessage.responseText);
+    errorDiv.append(p);
   });
 }
 
-document.getElementById("display-btn").addEventListener("click", getBikesWithin);
+document.getElementById("userInput-form").addEventListener("submit", getBikesWithin);
