83c4ac3a018591601e9e04d869ce800f64871bdb
Your Name <tajempey@gmail.com>
2023-11-15 13:09:41 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-
 function getBikesWithin() {
-  let promise = new Promise(function(resolve, reject) {
+  let promise = new Promise(function (resolve, reject) {
     let request = new XMLHttpRequest();
     const url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=10&location=Chico%2C%20CA%2C%20US&distance=40&stolenness=proximity`;
-    request.open("GET", url, true);
-    request.send();
-    request.addEventListener("loadend", function() {
+    request.addEventListener("loadend", function () {
       const response = JSON.parse(this.responseText);
       if (this.status === 200) {
         resolve([response]);
@@ -16,15 +10,18 @@ function getBikesWithin() {
         reject([this, response]);
       }
     });
+    request.open("GET", url);
+    request.send();
   });
 
-  promise.then(function(response) {
+  promise.then(function (response) {
     console.log(response);
-    return response;
-  }, function(errorMessage) {
+    for (let i = 0; i < response[0].bikes.length; i++) {
+      document.getElementById("display-bike-manufacturer").append(response[0].bikes[i].manufacturer_name);
+    }
+  }, function (errorMessage) {
     console.log(errorMessage);
-    return errorMessage;
   });
 }
 
-
+document.getElementById("display-btn").addEventListener("click", getBikesWithin);
